d5211bcf38200d6fb686ea33912aaa9da91e5be1
info <info@info14.cshl.edu>
2019-10-27 17:20:02 -0400

    works to label new files, next to save patch

--- generateNewPatch.py
+++ generateNewPatch.py
@@ -1,14 +1,12 @@
 #! /usr/bin/env python3
 
+import sys
 import subprocess
 import glob
 from md5_module import load_md5
+import json
 
 fileList = glob.glob('*.fasta')
-
-
-
-fileDict = dict()
 
 def load_patch_dict(FileNameDictionary):
 	for indivFile in fileList:
@@ -19,8 +17,13 @@ def load_patch_dict(FileNameDictionary):
 			else:
 				print(indivFile, "has changed")
 		else:
+			FileNameDictionary[indivFile] = load_md5(indivFile)
 			print("new file", indivFile)
+	print(FileNameDictionary)
 
-if __name__ == '__main__'
-	with open(sys.argv[1], "r") as FND:
-		load_patch_dict(FND)
+if __name__ == '__main__':
+	fnd = json.load(open(sys.argv[1], "r"))
+
+	with open(sys.argv[2], "w") as newdict:
+		newdict = load_patch_dict(fnd)
+
