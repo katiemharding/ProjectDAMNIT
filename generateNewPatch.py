96f935d7b5f15b898ed5a075b4afd76455ab7182
info <info@info14.cshl.edu>
2019-10-27 16:05:48 -0400

    adding worked code

--- generateNewPatch.py
+++ generateNewPatch.py
@@ -2,20 +2,25 @@
 
 import subprocess
 import glob
+from md5_module import load_md5
 
 fileList = glob.glob('*.fasta')
 
+
+
 fileDict = dict()
-for indvFile in fileList:
-	if indivFile in fileDict:
-		#newCheckSum = getmd5(indivFile)
-		if newCheckSum == fileDict[indivFile]
-			print("no change to", indivFile)
-		else
 
-		print(indivFile, "has changed")
-	else:
-		#fileDict[indivFile] = getmd5(indivFile)
-		print("new file", indivFile)
+def load_patch_dict(FileNameDictionary):
+	for indivFile in fileList:
+		if indivFile in FileNameDictionary:
+			newCheckSum = load_md5(indivFile)
+			if newCheckSum == FileNameDictionary[indivFile]:
+				print("no change to", indivFile)
+			else:
+				print(indivFile, "has changed")
+		else:
+			print("new file", indivFile)
 
-		
+if __name__ == '__main__'
+	with open(sys.argv[1], "r") as FND:
+		load_patch_dict(FND)
