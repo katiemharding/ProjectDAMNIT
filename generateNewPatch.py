efab70b1b2ce1685d464fe242a0cea104d1a7b4b
info <info@info14.cshl.edu>
2019-10-27 20:14:34 -0400

    diff function added

--- generateNewPatch.py
+++ generateNewPatch.py
@@ -6,24 +6,37 @@ import glob
 from md5_module import load_md5
 import json
 
-fileList = glob.glob('*.fasta')
+
+def find_diff(new_file, old_file):
+	file_diff = subprocess.run(['diff', new_file, old_file], stdout = subprocess.PIPE)
+	file_stdout = file_diff.stdout
+	diff_list = file_stdout.split()
+	return diff_list 
+# the code in the command line: diff --unified=0 new_file, old_file
+
 
 def load_patch_dict(FileNameDictionary):
 	for indivFile in fileList:
 		if indivFile in FileNameDictionary:
-			newCheckSum = load_md5(indivFile)
-			if newCheckSum == FileNameDictionary[indivFile]:
+			newMD5 = load_md5(indivFile) # find the md5 of the new file
+			oldMD5 = FileNameDictionary[indivFile] # for ease of reading, rename the file
+			if newMD5 == oldMD5: # if the new md5 is the same as the old md5 do nothing.
 				print("no change to", indivFile)
 			else:
-				print(indivFile, "has changed")
-		else:
-			FileNameDictionary[indivFile] = load_md5(indivFile)
-			print("new file", indivFile)
+				difference = find_diff(indivFile, ('.damnit/' + oldMD5)) # this is the patch
+				md5Graph_NewToOld[newMD5] = oldMD5 # add in the new md5 as a key, the old md5 is the value
+
+				print(indivFile,difference, "has changed")
+		else: # now the file name is not tracked, and it is not in the dictionary
+			newFilemd5 = load_md5(indivFile) # for this new file get a new md5
+			FileNameDictionary[indivFile] = newFilemd5 # add it to the file name dictionary
+			md5Graph_NewToOld = {} # this is the md5 dictionary with the path and the md5
+			md5Graph_NewToOld[newFilemd5] = "none" # since there are no changes, the first value will be none
+			print("new file", indivFile) 
 	print(FileNameDictionary)
 
 if __name__ == '__main__':
 	fnd = json.load(open(sys.argv[1], "r"))
-
+	fileList = glob.glob('*.fasta')
 	with open(sys.argv[2], "w") as newdict:
 		newdict = load_patch_dict(fnd)
-
