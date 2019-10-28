c761a9b89bd336e4605b8e948d21a8f31519bc44
info <info@info14.cshl.edu>
2019-10-28 14:57:30 -0400

    begining error handling

---
+++ retreiveFile.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import sys
+import json
+
+
+DamnitPath = '.damnit/'
+md5Graph_NewToOld_location = DamnitPath + 'md5Graph_NewToOld.json'
+FileNameDictionary_location = DamnitPath + 'quickDict.json'
+
+File_Name = ''
+try:
+	File_Name = sys.argv[1]
+	print("User provided file: ", File_Name)
+	if not File_Name.endswith('.fasta'):
+		raise ValueError("not a .fasta file")
+	FileNameDictionary = json.load(open(FileNameDictionary_location, "r")) # open the file name dictionary
+	if File_Name in FileNameDictionary:
+		print("file found:", File_Name)
+		print(File_Name)
+
+except IndexError:
+	print("Please provide file Name")
+except IOError as ex:
+	print(File_Name, ": Doesn't exist, ", ex.strerror)
