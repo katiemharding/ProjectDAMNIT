c6b2a14c90c140f4b946637d770acc967be19ab3
Katie <katiemharding@gmail.com>
2019-10-28 20:10:08 +0000

    adding form paramers

--- test_cgi.py
+++ test_cgi.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import cgi
+import cgitb
+cgitb.enable()
 
 form = cgi.FieldStorage()  # parse form data
 print ("Content-type:text/html\n")  # header plus blank line
-
+print('<html><head>')
 print ('<title>Hello Word - First CGI Program</title>')  # add a title
 print ('</head>')
 print ('<body>')
-if not 'user' in form:
-	print('<h1>Who are you?</h1>')
-else:
-	print('<h1>Hello <i>%s</i>!</h1>' % cgi.escape(form['user'].value))
-</body>
-</html>
+print('<form method=POST action="cgi-bin/anotherfile.py">')
+print('NewFile <input type=text name=NewFile>')
+print('</form>')
+print('</body>')
+print('</html>')
 
