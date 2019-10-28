65664cc26acb76571a042f2d73c5f53eaebaaf5f
katiemharding <katiemharding@gmail.com>
2019-10-28 20:58:17 +0000

    searching for file

--- test_cgi.py
+++ test_cgi.py
@@ -7,19 +7,21 @@ form = cgi.FieldStorage()  # parse form data
 
 print ("Content-type:text/html\n")  # header plus blank line
 print('<html><head>')
-print ('<title>Project Damnit Input Form</title>')  # add a title
+print ('<title>Project Damnit Input Form</title><br>')  # add a title
 print ('</head>')
 print ('<body>')
 print('<form method=POST action="./test_cgi.py">')
-print('<h1>Please select a file to upload'
+print('<h1>Please select a file to upload</h1>')
 print('New File <input type=file name=NewFile>')
 print('<input type=submit value="Upload File", name=button>')
 print('</form>')
 if 'NewFile' in form:
     print('Hello<br>')
     if form['NewFile'].file:
+        print("A")
         for line in form['NewFile'].file:
             print(line)
+            print("B")
 print('</body>')
 print('</html>')
 
