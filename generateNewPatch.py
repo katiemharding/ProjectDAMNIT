9123bd76ad0ad6faa1bb2d6e87c087acdc6fe08a
info <info@info01.cshl.edu>
2019-10-29 14:54:55 -0400

    fixed bugs

--- generateNewPatch.py
+++ generateNewPatch.py
@@ -9,14 +9,12 @@ import shutil
 from damnit_IO import read_json
 from damnit_IO import write_json
 
-DamnitPath = '.damnit/'
-md5Graph_NewToOld_location = DamnitPath + 'md5Graph_NewToOld.json'
-FileNameDictionary_location = DamnitPath + 'filenames_dict.json'
 
-def find_diff(new_file, old_file):
+
+def find_diff(new_file, old_file, damnit_path='.damnit'):
 	oldMD5 = load_md5(old_file)
-	string = "diff {} {} > {}.patch"
-	command = string.format(new_file,old_file,oldMD5)
+	string = "diff {} {} > {}/{}.patch"
+	command = string.format(new_file,old_file,damnit_path,oldMD5)
 	subprocess.run(command, shell = True)
 # the code in the command line: diff new_file old_file >filename
 # apparently we don't need --unified=0
@@ -39,14 +37,14 @@ def load_patch_dict(indivFile, damnit_path = '.damnit'):
 	    # 4) copy the file to DamnitPath directory the name of this file is the MD5
 	    # 5) copy the Patch to the DamnitPath directory, the name is the old MD5
 	    # !!!!!!!!!!! this will overwright the old file, and all the data in it. !!!!!!!!
-            difference = find_diff(indivFile, (DamnitPath + oldMD5)) # this is the patch
-            shutil.copyfile(indivFile, (DamnitPath+newMD5))
-            shutil.copyfile((oldMD5+".patch"), (DamnitPath + oldMD5))
+            difference = find_diff(indivFile, (damnit_path + '/' + oldMD5), damnit_path) # this is the patch
+            shutil.copyfile(indivFile, (damnit_path+ '/'+ newMD5))
+            #shutil.copyfile((oldMD5+".patch"), (damnit_path + '/' + oldMD5))
             md5Graph_NewToOld[newMD5] = oldMD5 # add in the new md5 as a key, the old md5 is the value
             FileNameDictionary[indivFile] = newMD5 # the file name is now linked to the new MD5 (old one lost)
-            with open(FileNameDictionary_location, "w") as newFileDict:
+            with open(damnit_path + "/filenames_dict.json", "w") as newFileDict:
                 json.dump(FileNameDictionary, newFileDict)
-            with open(md5Graph_NewToOld_location, "w") as newPatch:
+            with open(damnit_path + "/md5Graph_NewToOld.json", "w") as newPatch:
                 json.dump(md5Graph_NewToOld, newPatch) # open the file as write, save new dict
             print(indivFile, "has changed")
     else: # now the file name is not tracked, and it is not in the dictionary
@@ -55,28 +53,28 @@ def load_patch_dict(indivFile, damnit_path = '.damnit'):
 	# 2) save the new filename dictionary
 	# 3) add the file to the the graph dictionary.
 	# 4) save the graph dictionary
-        shutil.copyfile(indivFile, (DamnitPath+newMD5))
+        shutil.copyfile(indivFile, (damnit_path+'/'+newMD5))
         FileNameDictionary[indivFile] = newMD5 # add it to the file name dictionary
-        with open(FileNameDictionary_location, "w") as newFileDict:
+        with open(damnit_path + "/filenames_dict.json", "w") as newFileDict:
             json.dump(FileNameDictionary, newFileDict)
 	# old version created the graph file.  since it is now created we don't need it anymore
         md5Graph_NewToOld[newMD5] = "none" # since there are no changes, the first value will be none
-        with open(md5Graph_NewToOld_location, "w") as newPatch:
+        with open(damnit_path + "/md5Graph_NewToOld.json", "w") as newPatch:
             json.dump(md5Graph_NewToOld, newPatch) # open the file as write, save new dict
             print("new file", indivFile) 
 #    print(FileNameDictionary)
 
 if __name__ == '__main__':
-    try:
-        Usr_input = sys.argv[1]
-        print("User provided file: ", Usr_input)
-        load_patch_dict(Usr_input) # run the load patch function
+        DamnitPath = '.damnit/'
+        md5Graph_NewToOld_location = DamnitPath + 'md5Graph_NewToOld.json'
+        FileNameDictionary_location = DamnitPath + 'filenames_dict.json'
+        try:
+                Usr_input = sys.argv[1]
+                print("User provided file: ", Usr_input)
+                load_patch_dict(Usr_input) # run the load patch function
     
-    except IndexError:
-        fileList = glob.glob('*') # search for new files
-        for indivfile in fileList:
-            load_patch_dict(indivfile) # run the load patch function
-    #except:
-     #   print("fatal Error")
-      #  sys.exit(1)
+        except IndexError:
+                fileList = glob.glob('*') # search for new files
+                for indivfile in fileList:
+                        load_patch_dict(indivfile) # run the load patch function
 
