d6dd8d58cd6dc62d46ab18a80583c6d286c00659
info <info@info01.cshl.edu>
2019-10-29 15:11:55 -0400

    bug fix

--- init.py
+++ init.py
@@ -23,12 +23,23 @@ def all_file_name_dict(damnit_path = '.damnit'):
 	md5_dict = {}
 	md5Graph = {}
 	print('Adding files:')
+
+	# Create filenames file
+	fo_filenames = open(damnit_path + "/filenames_dict.json" , "w")
+	json.dump(filenames_dict, fo_filenames)
+	fo_filenames.close()
+
+	# Create md5 file
+	fo_md5 = open(damnit_path + "/md5_dict.json" , "w")
+	json.dump(md5_dict, fo_md5)
+	fo_md5.close()
+
 	for filename in filenames:
 		if os.path.isdir(filename):
 			continue
 		else:
 #			print(filename)
-			load_patch_dict(filename, filenames_dict, md5Graph)
+			load_patch_dict(filename, damnit_path)
 			md5 = load_md5(filename)
 			filenames_dict[filename] = md5
 			md5_dict[md5] = filename
@@ -43,9 +54,9 @@ def all_file_name_dict(damnit_path = '.damnit'):
 	json.dump(md5_dict, fo_md5)
 	fo_md5.close()
 
-	fo_Graph = open(damnit_path + "/md5Graph_NewToOld.json" ,"w")
-	json.dump(md5Graph, fo_Graph)
-	fo_Graph.close() 
+#	fo_Graph = open(damnit_path + "/md5Graph_NewToOld.json" ,"w")
+#	json.dump(md5Graph, fo_Graph)
+#	fo_Graph.close() 
 	
 	return all_file_name_dict
 
