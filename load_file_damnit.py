e7ff70df7b6d69c435fe4bd6a2f796b770fc9a89
katiemharding <katiemharding@gmail.com>
2019-10-29 12:56:40 +0000

    working again, had to go back a few steps

--- load_file_damnit.py
+++ load_file_damnit.py
@@ -14,28 +14,40 @@ print('<html><head>')
 print('<title>Project Damnit Input Form</title><br>')  # add a title
 print('</head>')
 print('<body>')
-print('<form method=POST enctype="multipart/form-data" action="./test_cgi.py">')
+print('<form method=POST enctype="multipart/form-data" action="./load_file_damnit.py">')
 print('<h1>Please select a file to upload</h1>')
 print('New File <input type=file name=NewFile>')
 print('<input type=submit value="Upload File", name=button>')
 print('</form>')
 
-fileitem = form['NewFile']
+if 'NewFile' in form:
+    print('Hello<br>')
+    if form['NewFile'].value:
+        print("did the file load?")
+        for line in form['NewFile'].value:
+            print(line)
+            print("file loaded")
+
+#fileitem = form['NewFile']
 
 # Test if the file was uploaded
-if fileitem.filename:
+#if fileitem.filename:
 
     # strip leading path from file name
     # to avoid directory traversal attacks
-    fn = os.path.basename(fileitem.filename)
-    open('/projects/damnit/share/.damnit/' + fn, 'wb').write(fileitem.file.read())
-    message = 'The file "' + fn + '" was uploaded successfully'
+ #   fn = os.path.basename(fileitem.filename)
+  
+  #FileNameDict = 'filenames_dict.json'
+   # open('/projects/damnit/share/.damnit/' + FileNameDict, 'r').write(fileitem.file.read())
+    
+    #message = 'The file "' + FileNameDict + '" was uploaded successfully'
 
 else:
     message = 'please select and submit a file'
     
 print("did the file load?<br>")
 print(message)
+print('<h1>Files already in storage</h1>')
 print('</body>')
 print('</html>')
 
